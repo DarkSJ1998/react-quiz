52ab91dccde3a9d1f09f8ea1d2c5aaea56c02da0
DarkSJ1998 <sarthakjn12@gmail.com>
2024-12-15 23:08:10 +0530

    Outsourced the answers related code to a different component

---
+++ src/components/Answers.jsx
@@ -0,0 +1,63 @@
+import { useRef } from 'react';
+
+export default function Answers({
+	answers,
+	selectedAnswer,
+	answerState,
+	onSelect,
+}) {
+	// A Ref so that we can persist the value across renders
+	const shuffledAnswers = useRef();
+
+	// Setting it on the first render cycle and not again
+	if (!shuffledAnswers.current) {
+		shuffledAnswers.current = [...answers];
+		shuffledAnswers.current.sort(() => Math.random() - 0.5);
+	}
+
+	return (
+		<ul id='answers'>
+			{shuffledAnswers.current.map((option, index) => {
+				// The dynamic CSS class that would be applied
+				let cssClass = '';
+
+				/**
+				 * Fetching the previous question's answer, ie, the
+				 * question that is being currently displayed
+				 */
+				const prevAnswer = selectedAnswer;
+
+				// Only for the current selected option
+				if (prevAnswer === option) {
+					switch (answerState) {
+						// If the option is in the selection phase
+						case 'answered':
+							cssClass = 'selected';
+							break;
+
+						// If the option is correct/wrong
+						case 'correct':
+						case 'wrong':
+							cssClass = `${answerState}`;
+							break;
+					}
+
+					if (answerState === 'answered') {
+						cssClass = 'selected';
+					}
+				}
+
+				return (
+					<li key={`option_${index + 1}`} className='answer'>
+						<button
+							onClick={() => onSelect(option)}
+							className={cssClass}
+						>
+							{option}
+						</button>
+					</li>
+				);
+			})}
+		</ul>
+	);
+}
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,15 +1,13 @@
-import { useCallback, useRef, useState } from 'react';
+import { useCallback, useState } from 'react';
 
 import quizCompletePng from '../assets/quiz-complete.png';
 import QUESTIONS from '../questions';
+import Answers from './Answers';
 import QuestionTimer from './QuestionTimer';
 
 export default function Quiz() {
 	const [answerState, setAnswerState] = useState('');
 	const [userAnswers, setUserAnswers] = useState([]);
-
-	// A Ref so that we can persist the value across renders
-	const shuffledAnswers = useRef();
 
 	/**
 	 * If the current question is unanswered, we will show that,
@@ -61,12 +59,6 @@ export default function Quiz() {
 		);
 	}
 
-	// Setting it on the first render cycle and not again
-	if (!shuffledAnswers.current) {
-		shuffledAnswers.current = [...QUESTIONS[activeQuestionIndex].answers];
-		shuffledAnswers.current.sort(() => Math.random() - 0.5);
-	}
-
 	return (
 		<div id='quiz'>
 			<div id='question'>
@@ -78,49 +70,13 @@ export default function Quiz() {
 
 				<h2>{QUESTIONS[activeQuestionIndex].text}</h2>
 
-				<ul id='answers'>
-					{shuffledAnswers.current.map((option, index) => {
-						// The dynamic CSS class that would be applied
-						let cssClass = '';
-
-						/**
-						 * Fetching the previous question's answer, ie, the
-						 * question that is being currently displayed
-						 */
-						const prevAnswer = userAnswers[userAnswers.length - 1];
-
-						// Only for the current selected option
-						if (prevAnswer === option) {
-							switch (answerState) {
-								// If the option is in the selection phase
-								case 'answered':
-									cssClass = 'selected';
-									break;
-
-								// If the option is correct/wrong
-								case 'correct':
-								case 'wrong':
-									cssClass = `${answerState}`;
-									break;
-							}
-
-							if (answerState === 'answered') {
-								cssClass = 'selected';
-							}
-						}
-
-						return (
-							<li key={`option_${index + 1}`} className='answer'>
-								<button
-									onClick={() => handleSelectAnswer(option)}
-									className={cssClass}
-								>
-									{option}
-								</button>
-							</li>
-						);
-					})}
-				</ul>
+				<Answers
+					key={activeQuestionIndex}
+					answers={QUESTIONS[activeQuestionIndex].answers}
+					selectedAnswer={userAnswers[userAnswers.length - 1]}
+					answerState={answerState}
+					onSelect={handleSelectAnswer}
+				/>
 			</div>
 		</div>
 	);
